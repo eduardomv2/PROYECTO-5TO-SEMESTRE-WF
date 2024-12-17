15fa7428814a4bf5fbc978528e70a5ddb81a0f18
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-12-17 01:54:07 -0600

    gerente

---
+++ PROYECTO 5TO SEMESTRE/Gerente/GerenteForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace PROYECTO_5TO_SEMESTRE
+{
+    partial class GerenteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelMarcaId = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBoxEstatusGerente = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxVehiculoGerente = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDuracionGerente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnModificarPruebaGerente = new System.Windows.Forms.Button();
+            this.btnAgregarPruebaGerente = new System.Windows.Forms.Button();
+            this.dataGridViewPruebasGerente = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxObservacionesGerente = new System.Windows.Forms.TextBox();
+            this.comboBoxClienteGerente = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPruebasGerente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1234, 558);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBoxClienteGerente);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBoxObservacionesGerente);
+            this.tabPage1.Controls.Add(this.labelMarcaId);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.comboBoxEstatusGerente);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.comboBoxVehiculoGerente);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBoxDuracionGerente);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnModificarPruebaGerente);
+            this.tabPage1.Controls.Add(this.btnAgregarPruebaGerente);
+            this.tabPage1.Controls.Add(this.dataGridViewPruebasGerente);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1226, 532);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // labelMarcaId
+            // 
+            this.labelMarcaId.AutoSize = true;
+            this.labelMarcaId.Location = new System.Drawing.Point(18, 17);
+            this.labelMarcaId.Name = "labelMarcaId";
+            this.labelMarcaId.Size = new System.Drawing.Size(35, 13);
+            this.labelMarcaId.TabIndex = 19;
+            this.labelMarcaId.Text = "label1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(61, 333);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "label8";
+            // 
+            // comboBoxEstatusGerente
+            // 
+            this.comboBoxEstatusGerente.FormattingEnabled = true;
+            this.comboBoxEstatusGerente.Location = new System.Drawing.Point(64, 349);
+            this.comboBoxEstatusGerente.Name = "comboBoxEstatusGerente";
+            this.comboBoxEstatusGerente.Size = new System.Drawing.Size(247, 21);
+            this.comboBoxEstatusGerente.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 268);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // comboBoxVehiculoGerente
+            // 
+            this.comboBoxVehiculoGerente.FormattingEnabled = true;
+            this.comboBoxVehiculoGerente.Location = new System.Drawing.Point(64, 284);
+            this.comboBoxVehiculoGerente.Name = "comboBoxVehiculoGerente";
+            this.comboBoxVehiculoGerente.Size = new System.Drawing.Size(247, 21);
+            this.comboBoxVehiculoGerente.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // textBoxDuracionGerente
+            // 
+            this.textBoxDuracionGerente.Location = new System.Drawing.Point(61, 148);
+            this.textBoxDuracionGerente.Name = "textBoxDuracionGerente";
+            this.textBoxDuracionGerente.Size = new System.Drawing.Size(247, 20);
+            this.textBoxDuracionGerente.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // btnModificarPruebaGerente
+            // 
+            this.btnModificarPruebaGerente.Location = new System.Drawing.Point(178, 424);
+            this.btnModificarPruebaGerente.Name = "btnModificarPruebaGerente";
+            this.btnModificarPruebaGerente.Size = new System.Drawing.Size(112, 63);
+            this.btnModificarPruebaGerente.TabIndex = 4;
+            this.btnModificarPruebaGerente.Text = "button2";
+            this.btnModificarPruebaGerente.UseVisualStyleBackColor = true;
+            this.btnModificarPruebaGerente.Click += new System.EventHandler(this.btnModificarPruebaGerente_Click);
+            // 
+            // btnAgregarPruebaGerente
+            // 
+            this.btnAgregarPruebaGerente.Location = new System.Drawing.Point(64, 424);
+            this.btnAgregarPruebaGerente.Name = "btnAgregarPruebaGerente";
+            this.btnAgregarPruebaGerente.Size = new System.Drawing.Size(108, 63);
+            this.btnAgregarPruebaGerente.TabIndex = 3;
+            this.btnAgregarPruebaGerente.Text = "button1";
+            this.btnAgregarPruebaGerente.UseVisualStyleBackColor = true;
+            this.btnAgregarPruebaGerente.Click += new System.EventHandler(this.btnAgregarPruebaGerente_Click);
+            // 
+            // dataGridViewPruebasGerente
+            // 
+            this.dataGridViewPruebasGerente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPruebasGerente.Location = new System.Drawing.Point(391, 17);
+            this.dataGridViewPruebasGerente.Name = "dataGridViewPruebasGerente";
+            this.dataGridViewPruebasGerente.Size = new System.Drawing.Size(814, 502);
+            this.dataGridViewPruebasGerente.TabIndex = 0;
+            this.dataGridViewPruebasGerente.SelectionChanged += new System.EventHandler(this.dataGridViewPruebasGerente_SelectionChanged);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1226, 532);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "label1";
+            // 
+            // textBoxObservacionesGerente
+            // 
+            this.textBoxObservacionesGerente.Location = new System.Drawing.Point(64, 211);
+            this.textBoxObservacionesGerente.Name = "textBoxObservacionesGerente";
+            this.textBoxObservacionesGerente.Size = new System.Drawing.Size(247, 20);
+            this.textBoxObservacionesGerente.TabIndex = 20;
+            // 
+            // comboBoxClienteGerente
+            // 
+            this.comboBoxClienteGerente.FormattingEnabled = true;
+            this.comboBoxClienteGerente.Location = new System.Drawing.Point(61, 96);
+            this.comboBoxClienteGerente.Name = "comboBoxClienteGerente";
+            this.comboBoxClienteGerente.Size = new System.Drawing.Size(247, 21);
+            this.comboBoxClienteGerente.TabIndex = 22;
+            // 
+            // GerenteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 582);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "GerenteForm";
+            this.Text = "GerenteForm";
+            this.Load += new System.EventHandler(this.GerenteForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPruebasGerente)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label labelMarcaId;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBoxEstatusGerente;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxVehiculoGerente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDuracionGerente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnModificarPruebaGerente;
+        private System.Windows.Forms.Button btnAgregarPruebaGerente;
+        private System.Windows.Forms.DataGridView dataGridViewPruebasGerente;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxObservacionesGerente;
+        private System.Windows.Forms.ComboBox comboBoxClienteGerente;
+    }
+}
---
+++ PROYECTO 5TO SEMESTRE/Gerente/GerenteForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTO_5TO_SEMESTRE
+{
+    public partial class GerenteForm : Form
+    {
+        string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=TESTING_DB;Integrated Security=True";
+
+
+        public GerenteForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GerenteForm_Load(object sender, EventArgs e)
+        {
+            // Cargar valores en el ComboBox de Estatus
+            comboBoxEstatusGerente.Items.Add("Activo");
+            comboBoxEstatusGerente.Items.Add("Inactivo");
+            comboBoxEstatusGerente.SelectedIndex = 0;
+
+            // Cargar Vehículos y Pruebas en sus respectivos controles
+            LoadComboBoxVehiculos();
+            CargarPruebasEnDataGridView();
+            LoadComboBoxClientes(); 
+        }
+
+        private void LoadComboBoxClientes()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT id_cliente, CONCAT(nombre, ' ', apellido) AS nombre_completo FROM Cliente WHERE estatus = 1";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    comboBoxClienteGerente.DataSource = dt;
+                    comboBoxClienteGerente.DisplayMember = "nombre_completo";
+                    comboBoxClienteGerente.ValueMember = "id_cliente";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los clientes: " + ex.Message);
+            }
+        }
+
+
+
+        private void LoadComboBoxVehiculos()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT id_vehiculo, nombre FROM Vehiculo WHERE cantidad > 0";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    comboBoxVehiculoGerente.DataSource = dt;
+                    comboBoxVehiculoGerente.DisplayMember = "nombre";
+                    comboBoxVehiculoGerente.ValueMember = "id_vehiculo";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los vehículos: " + ex.Message);
+            }
+        }
+
+        private void CargarPruebasEnDataGridView()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = @"
+                SELECT p.id_prueba AS [ID],
+                       v.nombre AS [Vehículo],
+                       p.nombre_cliente AS [Cliente],
+                       p.fecha_prueba AS [Fecha de Prueba],
+                       p.duracion_minutos AS [Duración (minutos)],
+                       p.observaciones AS [Observaciones],
+                       CASE p.estatus WHEN 1 THEN 'Activo' ELSE 'Inactivo' END AS [Estatus]
+                FROM PruebaVehiculo p
+                INNER JOIN Vehiculo v ON p.id_vehiculo = v.id_vehiculo";
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridViewPruebasGerente.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar las pruebas: " + ex.Message);
+            }
+        }
+
+        private void btnAgregarPruebaGerente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBoxClienteGerente.SelectedValue == null ||
+                    !int.TryParse(textBoxDuracionGerente.Text, out int duracion))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos y asegúrese de que la duración sea un número válido.");
+                    return;
+                }
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = @"INSERT INTO PruebaVehiculo (id_vehiculo, nombre_cliente, duracion_minutos, observaciones, estatus)
+                             VALUES (@id_vehiculo, @id_cliente, @duracion, @observaciones, @estatus)";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id_vehiculo", comboBoxVehiculoGerente.SelectedValue);
+                    cmd.Parameters.AddWithValue("@id_cliente", comboBoxClienteGerente.SelectedValue);
+                    cmd.Parameters.AddWithValue("@duracion", duracion);
+                    cmd.Parameters.AddWithValue("@observaciones", textBoxObservacionesGerente.Text);
+                    cmd.Parameters.AddWithValue("@estatus", comboBoxEstatusGerente.SelectedItem.ToString() == "Activo" ? 1 : 0);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Prueba de manejo agregada correctamente.");
+                    CargarPruebasEnDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar la prueba: " + ex.Message);
+            }
+        }
+
+        private void btnModificarPruebaGerente_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewPruebasGerente.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecciona una prueba para modificar.");
+                return;
+            }
+
+            try
+            {
+                // Validaciones
+                if (comboBoxClienteGerente.SelectedValue == null ||
+                    comboBoxVehiculoGerente.SelectedValue == null ||
+                    !int.TryParse(textBoxDuracionGerente.Text, out int duracion))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos correctamente.");
+                    return;
+                }
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = @"UPDATE PruebaVehiculo 
+                             SET id_vehiculo = @id_vehiculo, 
+                                 nombre_cliente = @id_cliente, 
+                                 duracion_minutos = @duracion, 
+                                 observaciones = @observaciones,
+                                 estatus = @estatus
+                             WHERE id_prueba = @id";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", dataGridViewPruebasGerente.SelectedRows[0].Cells["ID"].Value);
+                    cmd.Parameters.AddWithValue("@id_vehiculo", comboBoxVehiculoGerente.SelectedValue);
+                    cmd.Parameters.AddWithValue("@id_cliente", comboBoxClienteGerente.SelectedValue);
+                    cmd.Parameters.AddWithValue("@duracion", duracion);
+                    cmd.Parameters.AddWithValue("@observaciones", textBoxObservacionesGerente.Text);
+                    cmd.Parameters.AddWithValue("@estatus", comboBoxEstatusGerente.SelectedItem.ToString() == "Activo" ? 1 : 0);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Prueba modificada correctamente.");
+                    CargarPruebasEnDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al modificar la prueba: " + ex.Message);
+            }
+        }
+
+        private void dataGridViewPruebasGerente_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridViewPruebasGerente.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dataGridViewPruebasGerente.SelectedRows[0];
+
+                comboBoxVehiculoGerente.SelectedValue = row.Cells["Vehículo"].Value.ToString();
+                comboBoxClienteGerente.SelectedValue = row.Cells["Cliente"].Value.ToString();
+                textBoxDuracionGerente.Text = row.Cells["Duración (minutos)"].Value.ToString();
+                textBoxObservacionesGerente.Text = row.Cells["Observaciones"].Value.ToString();
+                comboBoxEstatusGerente.SelectedItem = row.Cells["Estatus"].Value.ToString();
+            }
+        }
+    }
+}
